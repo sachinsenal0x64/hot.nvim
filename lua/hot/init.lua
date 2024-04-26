00ebd03d0de04935d4c1c0911b28daac38ff19cd
0x64 <sachinsenal0x64@users.noreply.github.com>
2024-04-26 02:09:52 +0200

    💤 Debug

--- lua/hot/init.lua
+++ lua/hot/init.lua
@@ -116,26 +116,30 @@ local function stop()
 end
 
 -- Recursive function to search for the main file in the directory and its subdirectories
-local function find_main_file(directory, extensions)
-	for _, file in ipairs(vim.fn.readdir(directory)) do
-		local path = directory .. "/" .. file
-		if vim.fn.isdirectory(path) == 3 then
-			-- If it's a directory, recursively search inside it
-			local main_file = find_main_file(path, extensions)
-			if main_file then
-				return main_file
-			end
-		else
-			-- Check if the file name matches any of the extensions
-			for _, ext in ipairs(extensions) do
-				if file == "main" .. ext then
-					return path
-				elseif file == opts.tweaks.custom_file .. ext then
-					return path
+
+local function find_main_file(root_directory, extensions)
+	local function recursive_search(directory)
+		for _, file in ipairs(vim.fn.readdir(directory)) do
+			local path = directory .. "/" .. file
+			if vim.fn.isdirectory(path) == 1 then
+				-- If it's a directory, recursively search inside it
+				local main_file = recursive_search(path)
+				if main_file then
+					return main_file
+				end
+			else
+				-- Check if the file name matches any of the extensions
+				for _, ext in ipairs(extensions) do
+					if file == "main" .. ext then
+						return path
+					end
 				end
 			end
 		end
+		return nil
 	end
+
+	return recursive_search(root_directory)
 end
 
 local function restart()
